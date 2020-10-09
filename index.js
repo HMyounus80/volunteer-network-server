62251f8c99163264a6a2b8f123217787718fc247
HMyounus80 <younusm137@gmail.com>
2020-10-09 20:37:48 +0600

    nodemon and port fix

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}))
 
-
+app.get('/', (req, res) => {
+  res.send('Hello I am a Database')
+})
 
 
 const port = 5000
@@ -23,37 +25,58 @@ const port = 5000
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
-  const volunteerActivities = client.db("volunteerTasks").collection("volunteerActivities");
-  const addEvents = client.db("volunteerTasks").collection("addEvents");
+  const volunteerCollection = client.db("volunteerTasks").collection("volunteerActivities");
+  const eventListCollection = client.db("volunteerTasks").collection("eventList");
   
-  app.post('/addEvent', (req, res) => {
-      const eventOrder = req.body;
-      console.log(eventOrder)
-      addEvents.insertOne(eventOrder)
-      .then(result => {
-          res.send(result.insertedCount > 0)
-      })
-  })
-
-  app.get('/taskEvents', (req, res) => {
-    addEvents.find({email: req.query.email})
-   .toArray((err, documents) => {
-     console.log(documents)
-     res.send(documents)
-   })
-  })
-
-  app.delete('event-cancel', (req, res) => {
-    addEvents.deleteOne({_id:ObjectId(req.headers.id)})
-    .then(result => {
-      res.send(result.deletedCount > 0)
+    app.post('/addVolunteerEvent', (req, res) => { 
+        const addEvents = req.body;
+        console.log(addEvents)
+        volunteerCollection.insertOne(addEvents)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
     })
+
+      app.get('/taskEvents', (req, res) => {
+        volunteerCollection.find({})
+        .toArray((err, documents) => {
+          console.log(documents)
+          res.send(documents)
+      })
+
+      })
+
+    
+     app.post('/addEvenRegister', (req, res) => {
+      const eventRegister = req.body;
+      eventListCollection.insertOne(eventRegister)
+          .then(result => {
+              res.send(result.insertedCount > 0)
+          })
   })
 
+  app.get('/eventRegister/:email', (req, res) => {
+    const eventUserEmail = req.params.email;
+     eventListCollection.find({ email: eventUserEmail })
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+})
 
 
+
+  app.delete('/eventCancel/:id', (req, res) => {
+     const id = req.params.id;
+      eventListCollection.deleteOne({ _id: objectId(req.params.id) })
+        .then((result) => {
+            res.send(result.deletedCount > 0)
+        })
+})
+
+
+ 
  
 });
 
 
-app.listen(process.env.PORT || port);
+app.listen( process.env.PORT || port);
